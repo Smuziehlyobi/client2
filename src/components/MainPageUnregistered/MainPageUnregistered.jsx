d5786d9877aa60fc23cddd6323b37fd76b17dd48
TrojanDll <60787064+TrojanDll@users.noreply.github.com>
2024-04-20 11:29:30 +0300

    feat: Unregistered page

---
+++ src/components/MainPageUnregistered/MainPageUnregistered.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import logo from "./../../assets/img/logo.svg";
+
+export default function MainPageUnregistered() {
+  return (
+    <Container>
+      <img src={logo} alt="" />
+    </Container>
+  );
+}
