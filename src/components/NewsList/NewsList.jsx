6869c4ac8eeb1aa8667b704631eaa7c59b4e83a8
TrojanDll <60787064+TrojanDll@users.noreply.github.com>
2024-04-21 12:23:09 +0300

    feat: started news getting

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import exampleImg from "./../../assets/img/example-img.jpg";
 import NewsItem from "../NewsItem/NewsItem";
 import styles from "./NewsList.module.css";
+import axios from "axios";
 
 export default function NewsList() {
   const newsList = [
@@ -24,6 +25,48 @@ export default function NewsList() {
       img: exampleImg,
     },
   ];
+
+  let newsXml;
+  let xmlDocument;
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://don24.ru/rss",
+      responseType: "stream",
+    })
+      .then(function (response) {
+        newsXml = response.data;
+        console.log(newsXml);
+        // resolve(newsXml);
+      })
+      .then(function (response) {
+        xmlDocument = new DOMParser().parseFromString(newsXml, "text/xml");
+        // resolve(xmlDocument);
+      })
+      .then(function (response) {
+        console.log(xmlDocument);
+      });
+  }, []);
+
+  // const xmlReference = `
+  //   <tutorials>
+  //     <tutorial>
+  //       <id>123</id>
+  //       <title>qwerty</title>
+  //     </tutorial>
+  //     <tutorial>
+  //       <id>123808</id>
+  //       <title>dfbfdbb</title>
+  //     </tutorial>
+  //   </tutorials>
+  // `;
+
+  // useEffect(() => {
+  //   const xmlDocument = new DOMParser().parseFromString(xmlReference, "text/xml");
+  //   console.log(xmlDocument);
+  // }, []);
+
   return (
     <div className={styles.newsWrapper}>
       <div className={styles.newsTitle}>Новости</div>
