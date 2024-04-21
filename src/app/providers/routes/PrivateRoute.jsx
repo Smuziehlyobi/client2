f0d9e5e0faccb03b9fa4f81d705024c9798a4e8d
Fundxx <fundiexx@gmail.com>
2024-04-21 08:51:34 +0300

    privateRoutes

--- src/app/providers/routes/PrivateRoute.jsx
+++ src/app/providers/routes/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import {useAppSelector} from "../Store/hooks.js";
 import {Navigate} from "react-router";
 import {selectAuth} from "../../../features/auth/authSlice.js";
+import {appRoutes, RoutePaths} from "./routeConfig.jsx";
 
 const PrivateRoute = ({ children }) => {
   const { isLoggedIn } = useAppSelector(selectAuth)
   if (!isLoggedIn) {
     // not logged in so redirect to login page with the return url
-    return <Navigate to="/login" state={{ from: history.location }} />
+    return <Navigate to={RoutePaths[appRoutes.UNREGISTERED]} state={{ from: history.location }} />
   }
 
   // authorized so return child components
